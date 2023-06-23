479cf8c5628289630829a4ee6516d750e2b1e287
naiiiden <na4ko359@gmail.com>
2023-06-23 19:15:51 +0300

    direction aware hover

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
+let cursor = document.querySelector('.cursor')
+
 document.addEventListener('mousemove', (e) => {
-    let cursor = document.querySelector('.cursor');
-    cursor.style.left = (e.clientX - 7.5) + 'px';
-    cursor.style.top = (e.clientY - 2.5) + 'px'; 
-});
+    cursor.style.left = (e.clientX - 7.5) + 'px'
+    cursor.style.top = (e.clientY - 2.5) + 'px' 
+})
+
+document.querySelectorAll('a[href]').forEach(link => {
+    link.addEventListener('mouseenter', (e) => {
+        link.classList.add('hover')
+        cursor.classList.remove('cursor')
+    })
+
+    link.addEventListener('mouseleave', (e) => {
+        link.classList.remove('hover')
+        cursor.classList.add('cursor')
+    })
+})
