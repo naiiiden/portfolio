589c2559bdd5d17729b5342cf23ddedc6d46f99e
naiiiden <na4ko359@gmail.com>
2023-06-23 14:03:06 +0300

    custom cursor

--- script/script.js
+++ script/script.js
@@ -1,1 +1,5 @@
-console.log(1);
+document.addEventListener('mousemove', (e) => {
+    let cursor = document.querySelector('.cursor');
+    cursor.style.left = (e.clientX - 7.5) + 'px';
+    cursor.style.top = (e.clientY - 2.5) + 'px'; 
+});
