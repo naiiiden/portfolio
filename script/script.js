776d599b40d1caea90dff6695af13f537708849b
naiiiden <na4ko359@gmail.com>
2023-06-29 08:22:15 +0300

    canvas improvements

--- script/script.js
+++ script/script.js
@@ -54,83 +54,85 @@ function svgStrokeColor() {
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', svgStrokeColor)
 
-// ChatGPT
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-// TODO
-window.addEventListener("resize", function(){
-    canvas.setAttribute("width", window.innerWidth)
-    canvas.setAttribute("height", window.innerHeight)
-    console.log(canvas.width)
-    console.log(canvas.height)
-})
-
-
-let isDrawing = false
-let points = []
-
-canvas.addEventListener('mousedown', startDrawing)
-canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mouseup', stopDrawing)
-document.addEventListener('mouseleave', stopDrawing)
-
-function startDrawing(e) {
-  isDrawing = true
-  points.push({ x: e.offsetX, y: e.offsetY })
-  ctx.beginPath()
-  ctx.moveTo(e.offsetX, e.offsetY)
-}
-
-function draw(e) {
-  if (!isDrawing) return
-  points.push({ x: e.offsetX, y: e.offsetY })
-  ctx.lineTo(e.offsetX, e.offsetY)
-  ctx.stroke()
-  ctx.lineWidth = 0.5
-  ctx.strokeStyle = getStrokeStyle()
-}
-
-function stopDrawing() {
-  isDrawing = false
-  points.push(null)
-}
-
-function getStrokeStyle() {
-  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-  return isDark ? '#fff' : '#000'
-}
-
-function updateStrokeStyle() {
-  const strokeStyle = getStrokeStyle()
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  let drawingStarted = false
-
-  for (let i = 0; i < points.length; i++) {
-    const point = points[i]
-
-    if (point === null) {
-      drawingStarted = false
-      continue
-    }
-
-    const { x, y } = point
-
-    if (!drawingStarted) {
-      ctx.beginPath()
-      ctx.moveTo(x, y)
-      drawingStarted = true
-    } else {
-      ctx.lineTo(x, y)
-      ctx.stroke()
-    }
-
-    ctx.strokeStyle = strokeStyle
+if (window.matchMedia('(pointer: fine)').matches) {
+  // ChatGPT
+  const canvas = document.querySelector('canvas')
+  const ctx = canvas.getContext('2d')
+  
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  
+  // TODO
+  window.addEventListener("resize", function(){
+      canvas.setAttribute("width", window.innerWidth)
+      canvas.setAttribute("height", window.innerHeight)
+      console.log(canvas.width)
+      console.log(canvas.height)
+  })
+  
+  
+  let isDrawing = false
+  let points = []
+  
+  canvas.addEventListener('mousedown', startDrawing)
+  canvas.addEventListener('mousemove', draw)
+  canvas.addEventListener('mouseup', stopDrawing)
+  document.addEventListener('mouseleave', stopDrawing)
+  
+  function startDrawing(e) {
+    isDrawing = true
+    points.push({ x: e.offsetX, y: e.offsetY })
+    ctx.beginPath()
+    ctx.moveTo(e.offsetX, e.offsetY)
   }
+  
+  function draw(e) {
+    if (!isDrawing) return
+    points.push({ x: e.offsetX, y: e.offsetY })
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.stroke()
+    ctx.lineWidth = 0.5
+    ctx.strokeStyle = getStrokeStyle()
+  }
+  
+  function stopDrawing() {
+    isDrawing = false
+    points.push(null)
+  }
+  
+  function getStrokeStyle() {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    return isDark ? '#fff' : '#000'
+  }
+  
+  function updateStrokeStyle() {
+    const strokeStyle = getStrokeStyle()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  
+    let drawingStarted = false
+  
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i]
+  
+      if (point === null) {
+        drawingStarted = false
+        continue
+      }
+  
+      const { x, y } = point
+  
+      if (!drawingStarted) {
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+        drawingStarted = true
+      } else {
+        ctx.lineTo(x, y)
+        ctx.stroke()
+      }
+  
+      ctx.strokeStyle = strokeStyle
+    }
+  }
+  
+  svgStrokeColor()
 }
-
-svgStrokeColor()
