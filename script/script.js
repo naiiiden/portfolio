d2783d549fb8cbd906b72ab5b7c54ef295147d7d
naiiiden <na4ko359@gmail.com>
2023-07-27 16:32:36 +0300

    theme toggle functionality

--- script/script.js
+++ script/script.js
@@ -210,3 +210,51 @@ repoLinks.forEach((repo, index) => {
     updateImageOpacity(-1);
   });
 });
+
+// https://web.dev/building-a-theme-switch-component/#javascript
+const onClick = () => {
+  theme.value = theme.value === 'light'
+      ? 'dark'
+      : 'light'
+  setPreference()
+}
+
+const getColorPreference = () => {
+  if (localStorage.getItem('theme-preference')) {
+      return localStorage.getItem('theme-preference')
+  } else {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light'
+  }
+}
+
+const setPreference = () => {
+  localStorage.setItem('theme-preference', theme.value)
+  reflectPreference()
+}
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute('data-theme', theme.value)
+  document.querySelector('.color-scheme-toggle')?.setAttribute('aria-label', `Toggle ${theme.value === 'dark' ? 'light' : 'dark'} mode`)
+  document.querySelector('.color-scheme-toggle img').setAttribute('src', theme.value === 'light' ? './images/moon.svg' :  './images/sun.svg')
+}
+
+const theme = {
+  value: getColorPreference(),
+}
+
+reflectPreference()
+
+window.onload = () => {
+  reflectPreference()
+
+  document.querySelector('.color-scheme-toggle').addEventListener('click', onClick)
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches: isDark }) => {
+  theme.value = isDark 
+      ? 'dark' 
+      : 'light'
+  setPreference()
+})
