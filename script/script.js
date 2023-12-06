63e323c55cce18b55529fe665747c75e23f38350
naiiiden <na4ko359@gmail.com>
2023-12-06 09:38:47 +0200

    add font

--- script/script.js
+++ script/script.js
@@ -1,29 +1,3 @@
-// const sections = document.querySelectorAll("section");
-// function setFlexGrow(section, value) {
-//   section.style.flexGrow = value;
-// }
-
-// function updateFlexGrow(targetSectionId) {
-//   if (!targetSectionId) {
-//     setFlexGrow(sections[0], "1");
-//   } else {
-//     sections.forEach((section) => setFlexGrow(section, "0"));
-//     const targetSection = document.getElementById(targetSectionId);
-//     if (targetSection) {
-//       setFlexGrow(targetSection, "1");
-//     }
-//   }
-// }
-
-// const targetSectionId = window.location.hash.slice(1);
-// updateFlexGrow(targetSectionId);
-
-// window.addEventListener("hashchange", () => {
-//   const targetSectionId = window.location.hash.slice(1);
-//   updateFlexGrow(targetSectionId);
-// });
-
-// ???
 if (window.location.hash === "") {
     window.location.hash = "#about";
 }
