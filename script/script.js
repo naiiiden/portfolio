aec4c110c74d74ac1b8889af410cd2672ff32560
naiiiden <na4ko359@gmail.com>
2023-12-06 15:09:26 +0200

    anim complete

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
 if (window.location.hash === "") {
     window.location.hash = "#about";
 }
+
+const verticalText = document.querySelector('.vertical-text span');
+
+verticalText.addEventListener("click", () => {
+    verticalText.classList.toggle('cyrilic');
+});
