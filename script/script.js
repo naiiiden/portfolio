2c2ae3a37d36e5c008c0c7abc5f349f76431a5f5
naiiiden <na4ko359@gmail.com>
2023-12-08 19:06:03 +0200

    img follow cursor

--- script/script.js
+++ script/script.js
@@ -9,6 +9,27 @@ verticalText.addEventListener("click", () => {
 });
 
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("#cursor").style.left = e.clientX - 10 + "px";
-  document.querySelector("#cursor").style.top = e.clientY - 10 + "px";
+    document.querySelector("#cursor").style.cssText = `left: ${e.clientX - 10}px; top: ${e.clientY - 10}px;`;
 });
+
+const listItems = document.querySelectorAll("#work-list li");
+
+listItems.forEach((item) => {
+  item.addEventListener("mousemove", moveImage);
+});
+
+function moveImage(event) {
+  const boundingRect = event.currentTarget.getBoundingClientRect();
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const imageCenterX = boundingRect.left + boundingRect.width / 2;
+  const imageCenterY = boundingRect.top + boundingRect.height / 2;
+
+  const moveX = mouseX - imageCenterX;
+  const moveY = mouseY - imageCenterY;
+
+  const transformValue = `translate(${moveX}px, ${moveY}px)`;
+
+  event.currentTarget.querySelector("img").style.transform = transformValue;
+}
