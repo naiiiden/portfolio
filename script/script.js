f6ae93d6c8a685beeb2d5694db9def20c9381203
naiiiden <na4ko359@gmail.com>
2023-12-08 19:45:07 +0200

    improve js

--- script/script.js
+++ script/script.js
@@ -15,21 +15,7 @@ document.addEventListener("mousemove", (e) => {
 const listItems = document.querySelectorAll("#work-list li");
 
 listItems.forEach((item) => {
-  item.addEventListener("mousemove", moveImage);
+  item.addEventListener("mousemove", (e) => {
+    e.currentTarget.querySelector("img").style.cssText = `left: ${e.clientX - 225}px; top: ${e.clientY - 250}px;`;
+  });
 });
-
-function moveImage(event) {
-  const boundingRect = event.currentTarget.getBoundingClientRect();
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-
-  const imageCenterX = boundingRect.left + boundingRect.width / 2;
-  const imageCenterY = boundingRect.top + boundingRect.height / 2;
-
-  const moveX = mouseX - imageCenterX;
-  const moveY = mouseY - imageCenterY;
-
-  const transformValue = `translate(${moveX}px, ${moveY}px)`;
-
-  event.currentTarget.querySelector("img").style.transform = transformValue;
-}
